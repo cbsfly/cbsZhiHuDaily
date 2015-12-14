9304fa1c0e6505bcda9ccf70dde0e762c7b0801c
a502332019 <cbs_950320@qq.com>
2015-12-14 16:39:46 +0800

    Initial Commit

---
+++ zhihucbs/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zhihucbs
+//
+//  Created by 陈秉慎 on 15/12/14.
+//  Copyright (c) 2015年 陈秉慎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ zhihucbs/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  zhihucbs
+//
+//  Created by 陈秉慎 on 15/12/14.
+//  Copyright (c) 2015年 陈秉慎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
